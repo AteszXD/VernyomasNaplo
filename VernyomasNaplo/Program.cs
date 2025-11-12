65cadcb58dce5264c932b6c1b1bf915b96990065
AteszXD <keria@kkszki.hu>
2025-11-12 20:29:13 +0100

    A beolvasás és a kiírás (képernyőre) alapja már megvan

--- VernyomasNaplo/Program.cs
+++ VernyomasNaplo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,25 @@ namespace VernyomasNaplo
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static List<string> records;
+        static void Main(string[] _)
         {
 
+        }
+
+        static void ReadCSVFile(string username)
+        {
+            // a username.csv fájl beolvasása, ez a létező eredményeknek van
+            records = File.ReadAllLines($"Users/{username}.csv").ToList();            
+        }
+
+        static void DisplayRecords()
+        {
+            Console.Write("|Név\t\t|Dátum\t\t\t\t|Vérnyomás\t\t|\n");
+            foreach (string record in records)
+            {
+                Console.WriteLine($"|{record.Split(';')[0]}\t|{record.Split(';')[1]}\t|{record.Split(';')[2]}\t|\n");
+            }
         }
     }
 }
