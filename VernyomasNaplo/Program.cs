82c5b29078dd3ea6f332b9850148693708f49b4f
irzol <kocsisr1@kkszki.hu>
2025-11-19 19:56:28 +0100

    sikeres admin fiók regisztrálásánál admin fiók azonalli beléptetése

--- VernyomasNaplo/Program.cs
+++ VernyomasNaplo/Program.cs
@@ -541,7 +541,16 @@ namespace VernyomasNaplo
             File.Create($"{username}.csv").Close();
             File.Move($"{username}.csv", $"Users/{username}.csv");
             user = username;
-            LoggedinMenu();
+            if (user == "admin")
+            {
+                LoggedinAdminMenu();
+                
+            }
+            else
+            {
+                LoggedinMenu();
+            }
+               
         }
 
         /// <summary>
